f38314008bb4b9ae1356823d0a0b2d63e6289c9f
platsch53 <156426481+platsch53@users.noreply.github.com>
2024-01-14 14:17:42 +0100

    Delete hello_world.cpp

--- hello_world.cpp
+++
@@ -1,9 +0,0 @@
-// hello_world.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
-}
